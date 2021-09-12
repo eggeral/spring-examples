4c1d359eac9df1748308c6a6138221ca12c7bd3f
Alexander Egger <alexander.egger@egger.software>
2021-09-12 22:50:51 +0200

    Show basic testing implemantations

--- src/test/kotlin/egger/software/spring/ApplicationTests.kt
+++ src/test/kotlin/egger/software/spring/ApplicationTests.kt
@@ -1,13 +1,35 @@
 package egger.software.spring
 
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+    @LocalServerPort
+    private val port = 0
+
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
+
     @Test
-    fun contextLoads() {
+    fun `it should start the server and make the REST service available`() {
+        assertThat(
+            restTemplate.getForObject(
+                "http://localhost:$port/hello?name=Test",
+                String::class.java
+            ),
+            `is`(
+                """
+                {"counter":1,"name":"Hello, Test!"}
+                """.trimIndent()
+            )
+        )
     }
 
 }
