6a8314424cc7f625dbe74b8d3eb0444bc735c699
Alexander Egger <alexander.egger@egger.software>
2021-09-14 15:03:40 +0200

    Set JVM target version to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 
 group = "egger.software"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -28,7 +27,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 }
 
