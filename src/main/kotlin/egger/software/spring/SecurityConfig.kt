8084b737a13476200af007337a7282014acd82e7
Alexander Egger <alexander.egger@egger.software>
2021-09-21 16:47:38 +0200

    Add method security examples

--- src/main/kotlin/egger/software/spring/SecurityConfig.kt
+++ src/main/kotlin/egger/software/spring/SecurityConfig.kt
@@ -1,12 +1,16 @@
 package egger.software.spring
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
 
 
@@ -20,6 +24,8 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 authorize("/shop/**", hasAuthority("ROLE_USER"))
             }
             formLogin {  }
+            csrf { disable() } // to make post work
+            httpBasic {  }
         }
     }
 
@@ -31,9 +37,18 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             .roles("USER")
             .build()
 
-        return InMemoryUserDetailsManager(userDetails)
+        val adminDetails = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("password")
+            .roles("ADMIN")
+            .build()
+
+        return InMemoryUserDetailsManager(userDetails, adminDetails)
     }
 
 
 }
 
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+class MethodSecurityConfig : GlobalMethodSecurityConfiguration()
