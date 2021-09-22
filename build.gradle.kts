70909846c6058e9528919940cfb14657fa6ef012
Alexander Egger <alexander.egger@egger.software>
2021-09-22 22:38:04 +0200

    Add JDBC security examples

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("com.h2database:h2:1.4.200")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- src/main/kotlin/egger/software/spring/SecurityConfig.kt
+++ src/main/kotlin/egger/software/spring/SecurityConfig.kt
@@ -1,7 +1,10 @@
 package egger.software.spring
 
+import org.h2.tools.Server
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -9,13 +12,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.core.userdetails.User
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
+import java.sql.SQLException
+import javax.sql.DataSource
 
 
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(private val dataSource: DataSource) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http {
@@ -23,31 +29,46 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 authorize("/homepage/**", permitAll)
                 authorize("/shop/**", hasAuthority("ROLE_USER"))
             }
-            formLogin {  }
+            formLogin { }
             csrf { disable() } // to make post work
-            httpBasic {  }
+            httpBasic { }
         }
     }
 
-    @Bean
-    public fun userDetailsManager(): UserDetailsManager {
-        val userDetails = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
-            .roles("USER")
-            .build()
-
-        val adminDetails = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("password")
-            .roles("ADMIN")
-            .build()
-
-        return InMemoryUserDetailsManager(userDetails, adminDetails)
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()
+            .withUser(
+                User.withUsername("user")
+                    .password(passwordEncoder().encode("password"))
+                    .roles("USER")
+            ).withUser(
+                User.withUsername("admin")
+                    .password(passwordEncoder().encode("password"))
+                    .roles("ADMIN")
+            )
     }
 
+    @Bean
+    fun userDetailsManager(): UserDetailsManager {
+        return JdbcUserDetailsManager(dataSource)
+    }
 
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    fun inMemoryH2DatabaseaServer(): Server {
+        return Server.createTcpServer(
+            "-tcp", "-tcpAllowOthers", "-tcpPort", "9090"
+        )
+    }
 }
+
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
