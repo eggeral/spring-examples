76d950c9f78c976b4df9570c866a8290bae66237
Alexander Egger <alexander.egger@egger.software>
2021-09-25 09:35:25 +0200

    Add Spring DATA JPA examples

--- src/main/kotlin/egger/software/spring/FlightRepository.kt
+++ src/main/kotlin/egger/software/spring/FlightRepository.kt
@@ -1,18 +1,27 @@
 package egger.software.spring
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
+import javax.persistence.*
 
+@Entity
+data class Flight(
+    @Id
+    @GeneratedValue
+    val id: Long? = null,
+    val number: String,
 
-data class Flight(@Id val id: Long, val number: String, val from: String, val to: String)
+    @Column(name="\"FROM\"") // escape SQL keyword
+    val from: String,
+    val to: String
+)
 
 @Repository
 interface FlightRepository : PagingAndSortingRepository<Flight, Long> {
     fun findByNumber(number: String): List<Flight>
 
 
-    fun findByNumberAndFrom(number: String, from:String): List<Flight>
+    fun findByNumberAndFrom(number: String, from: String): List<Flight>
     fun findByNumberOrderById(number: String): List<Flight>
     fun findByNumberBetween(start: String, end: String): List<Flight>
 }
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into FLIGHT (NUMBER, "FROM", TO)
-values ('LH1234', 'VIE', 'FRA');
-
