8a8a7b7e09781c755339650491c988f7a35ce144
Alexander Egger <alexander.egger@egger.software>
2021-09-25 08:57:14 +0200

    Add Spring Data JDBC examples

--- src/main/kotlin/egger/software/spring/FlightRepository.kt
+++ src/main/kotlin/egger/software/spring/FlightRepository.kt
@@ -1,27 +1,18 @@
 package egger.software.spring
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
-import javax.persistence.*
 
-@Entity
-data class Flight(
-    @Id
-    @GeneratedValue
-    val id: Long? = null,
-    val number: String,
 
-    @Column(name="\"FROM\"") // escape SQL keyword
-    val from: String,
-    val to: String
-)
+data class Flight(@Id val id: Long, val number: String, val from: String, val to: String)
 
 @Repository
 interface FlightRepository : PagingAndSortingRepository<Flight, Long> {
     fun findByNumber(number: String): List<Flight>
 
 
-    fun findByNumberAndFrom(number: String, from: String): List<Flight>
+    fun findByNumberAndFrom(number: String, from:String): List<Flight>
     fun findByNumberOrderById(number: String): List<Flight>
     fun findByNumberBetween(start: String, end: String): List<Flight>
 }
