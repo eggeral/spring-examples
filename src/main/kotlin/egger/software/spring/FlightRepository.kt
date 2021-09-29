501ffe48458ece2752cdab596c73eeee1eb3b9fb
Alexander Egger <alexander.egger@egger.software>
2021-09-29 15:37:14 +0200

    Fix entity

--- src/main/kotlin/egger/software/spring/FlightRepository.kt
+++ src/main/kotlin/egger/software/spring/FlightRepository.kt
@@ -1,12 +1,25 @@
 package egger.software.spring
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import org.springframework.stereotype.Repository
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
 
 
-data class Flight(@Id val id: Long, val number: String, val from: String, val to: String)
+@Entity
+data class Flight(
+
+    @Id
+    @GeneratedValue
+    val id: Long? = null,
+    val number: String,
+
+    @Column(name="\"FROM\"") // escape SQL keyword
+    val from: String,
+    val to: String
+)
 
 @RepositoryRestResource
 interface FlightRepository : PagingAndSortingRepository<Flight, Long> {
