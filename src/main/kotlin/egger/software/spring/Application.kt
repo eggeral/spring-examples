8e08c49b48830b995bfd5fed2d441c63b1cc8beb
Alexander Egger <alexander.egger@egger.software>
2021-09-29 16:24:05 +0200

    Deploy example process

--- src/main/kotlin/egger/software/spring/Application.kt
+++ src/main/kotlin/egger/software/spring/Application.kt
@@ -1,9 +1,11 @@
 package egger.software.spring
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableProcessApplication
 class Application
 
 fun main(args: Array<String>) {
--- src/main/kotlin/egger/software/spring/JdbcConfig.kt
+++ src/main/kotlin/egger/software/spring/JdbcConfig.kt
@@ -12,15 +12,9 @@ import javax.sql.DataSource
 
 @Configuration
 class JdbcConfig {
-    @Bean
-    fun dataSource(): DataSource? {
-        return EmbeddedDatabaseBuilder()
-            .setType(EmbeddedDatabaseType.H2)
-            .setName("camunda").build()
-    }
 
     @Bean(initMethod = "start", destroyMethod = "stop")
-    fun inMemoryH2DatabaseaServer(): Server {
+    fun h2DatabaseServer(): Server {
         return Server.createTcpServer(
             "-tcp", "-tcpAllowOthers", "-tcpPort", "9090"
         )
